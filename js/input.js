e4c962399d6fd8ea3be34eb26aaf0c29dc424326
SerhiiMarcha <128225428+SerhiiMarcha@users.noreply.github.com>
2023-06-03 10:21:02 +0300

    Update input.js

--- js/input.js
+++ js/input.js
@@ -1,6 +1,13 @@
-let productName = "Droid";
-let pricePerItem = 2000;
-
-// Change code below this line
-console.productName = "Repair droid";
-console.pricePerItem = 3500;
+function checkStorage(available, ordered) {
+  let message;
+  // Change code below this line
+  if (ordered = 0) {
+    message = 'There are no products in the order!';
+  } else if (available>=ordered) { 
+    message = 'The order is accepted, our manager will contact you';
+  } else (available<ordered){
+   message = 'Your order is too large, there are not enough items in stock!';
+  }
+  // Change code above this line
+  return message;
+}
